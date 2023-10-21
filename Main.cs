72ea1d82ae66814d94eb9bf250ae97b30398379c
frostudio <m13706430527@hotmail.com>
2023-10-21 13:24:37 +0800

    添加项目文件。

---
+++ Main.cs
@@ -0,0 +1,42 @@
+﻿using AlgodooStudio.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPluginTemplate
+{
+    /// <summary>
+    /// 插件本体
+    /// </summary>
+    public sealed class Main : Plugin
+    {
+        public Main()
+        {
+            base.id = 0;
+            base.name = "PluginTemplate";
+            base.version = "1.0.0";
+        }
+
+        public override void OnLoad()
+        {
+
+        }
+
+        public override void OnEnabled()
+        {
+
+        }
+
+        public override void OnDisabled()
+        {
+
+        }
+
+        public override void OnUnload()
+        {
+
+        }
+    }
+}
